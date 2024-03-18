def4d583a15d3268c2bd6c7ea826eb8e3276585b
peggyp <proffit.peggy@yahoo.fr>
2024-03-18 21:36:16 +0100

    update component Home. Add img and h1

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import imageBanner from "../../assets/images/imageBannerHome.png";
+
 const Home = () => {
     return (
-        <div>
-          <h1>Page "Accueil"</h1>
+        <div className="home">
+            <div className="bannerHome">
+                <img src={imageBanner} alt="Image mer et rochers" />
+                <h1>Chez vous, partout et ailleurs</h1>
+            </div>
         </div>
     );
 };
