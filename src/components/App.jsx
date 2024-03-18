58e4edd3a248609fe2893d191c8730ae47ec021d
peggyp <proffit.peggy@yahoo.fr>
2024-03-18 21:34:03 +0100

    add component Footer in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Header from './Header';
+import Footer from './Footer';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 };
