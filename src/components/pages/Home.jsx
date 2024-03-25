150e369acadc488eb00776a8368d67910dc5449f
peggyp <proffit.peggy@yahoo.fr>
2024-03-25 16:09:36 +0100

    add component Thumb

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import imageBanner from "../../assets/images/imageBannerHome.png";
+import Thumb from './Thumb';
+import logements from '../../data/logements.json';
 
 const Home = () => {
     return (
@@ -7,6 +9,9 @@ const Home = () => {
             <div className="bannerHome">
                 <img src={imageBanner} alt="Image mer et rochers" />
                 <h1>Chez vous, partout et ailleurs</h1>
+            </div>
+            <div className="gallery">                
+                <Thumb logements={logements}/>
             </div>
         </div>
     );
