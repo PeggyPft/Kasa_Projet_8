cd0410bdc6a3ba7178f9fe61a98098a4c93ee2ac
peggyp <proffit.peggy@yahoo.fr>
2024-03-25 16:00:25 +0100

    update with responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import logoFooter from "../assets/images/logoFooter.png";
-import textFooter from "../assets/images/textFooter.png";
 
 const Footer = () => {
     return (
         <div className="footer">
             <img src={logoFooter} alt="logo Kasa" className='logoFooter'/>
-            <h3 className='textFooter'>© 2020 Kasa. All rights reserved</h3>
+            <p>@ 2020 Kasa. All rights reserved</p>
         </div>
     );
 };
