11c3a3fa149a68f8c1b680102aef47f46895d3e3
peggyp <proffit.peggy@yahoo.fr>
2024-03-31 19:41:10 +0200

    add component Collapse and update scss

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import imageBannerAbout from "../../assets/images/imageBannerAbout.png";
+import Collapse from './Collapse';
+import collapseData from "../../data/collapse.json";
 
 const About = () => {
     return (
@@ -7,6 +9,10 @@ const About = () => {
             <div className="bannerAbout">
                 <img src={imageBannerAbout} alt="Photo montagnes" />
             </div>
+            <div className="collapseContainer">
+                <Collapse notices={collapseData} />
+            </div>
+
         </div>
     );
 };
