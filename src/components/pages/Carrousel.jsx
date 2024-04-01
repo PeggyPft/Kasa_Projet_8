8facb99d84ba2286b6b2677bf56bbb93b5cab939
peggyp <proffit.peggy@yahoo.fr>
2024-04-01 19:33:55 +0200

    create carrousel

---
+++ src/components/pages/Carrousel.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import arrowLeft from '../../assets/images/arrow-left.svg';
+import arrowRight from '../../assets/images/arrow-right.svg';
+
+const Carrousel = ({pictures}) => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    
+    const previousImage = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === 0 ? pictures.length - 1 : prevIndex - 1
+        );
+    };
+
+    const nextImage = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === pictures.length - 1 ? 0 : prevIndex + 1
+        );
+    };
+
+
+
+    return (
+        <div className="carrousel">
+            <img src={arrowLeft} alt="flèche gauche" className="arrowLeft" onClick={previousImage}/>
+            <img src={pictures[currentImageIndex]} alt={`Image ${currentImageIndex + 1}`} className='pictureCarrousel' />
+            <img src={arrowRight} alt="flèche droite" className="arrowRight" onClick={nextImage} />
+            
+        </div>
+    );
+};
+
+Carrousel.propTypes = {
+    pictures: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Carrousel;
