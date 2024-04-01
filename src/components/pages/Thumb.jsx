86b3e04aff99a80c6a1d08e2a68e664e4e381ccc
peggyp <proffit.peggy@yahoo.fr>
2024-04-01 19:34:44 +0200

    update Thumb with Link

--- src/components/pages/Thumb.jsx
+++ src/components/pages/Thumb.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import { Link } from 'react-router-dom';
 
 const Thumb = ({logements}) => {
     return (
         logements.map((logement) => (
-            <div className="thumb" key={logement.id}>
+            <Link to={`/logement/${logement.id}`} className="thumb" key={logement.id}>
                 <img src={logement.cover} alt={logement.title} />
                 <h2 className='titleImg'>{logement.title}</h2>
-            </div>
+            </Link>
 
         ))
     );
