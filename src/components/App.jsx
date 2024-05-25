78f066b70d8448383cf37c0fefb712d793c403b9
peggyp <proffit.peggy@yahoo.fr>
2024-05-25 20:53:45 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Banner from './pages/Banner';
 import About from './pages/About';
 import Header from './Header';
 import Footer from './Footer';
